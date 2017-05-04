fd64ea6989e097498ee7d3b90043130a65c8d817
Cristian Ayub Chavira <CSnakeEyes@users.noreply.github.com>
2017-05-03 18:31:43 -0600

    Part II Changes
    Additional work towards our victory

--- Main.hs
+++ Main.hs
@@ -1,5 +1,3 @@
-module Main where
-
 import System.IO
 import System.Random
 import Board
@@ -10,42 +8,41 @@ import Board
 	-- number1 <- randomRIO (1,10) :: IO Int
 	-- putStrLn ("Your random number X is: " ++ show number)
 	-- putStrLn ("Your random number Y is: " ++ show number1)
-	
-placeShips :: [Int] -> [Row] -> [Row]
-placeShips [] board = [[]]
-placeShips (h:t) board
-	| (isShipPlaceable h x y dir board) = placeShip h x y dir board : placeShips t x y dir board
-	| otherwise = placeShips ships board where
-        x <- randomRIO (0,9) :: IO Int
-        y <- randomRIO (0,9) :: IO Int
-        ships = h ++ t
+
+-- x :: IO Int
+-- y :: IO Int
+-- placeShips :: [Int] -> [[Int]] -> IO [[Int]]
+-- placeShips [] board = [[]]
+-- placeShips (h:t) board
+	-- | (isShipPlaceable h x y dir board) = placeShip h x y dir board : placeShips t x y dir board
+	-- | otherwise = placeShips ships board where
+        -- x <- randomRIO (0,9) :: IO Int
+        -- y <- randomRIO (0,9) :: IO Int
+        -- ships = h ++ t
+
+placeShips :: [Int] -> [[Int]] -> IO [[Int]]
+placeShips (h:t) board = do 
+	x <- randomRIO (0,9) :: IO Int
+	y <- randomRIO (0,9) :: IO Int
+	-- d <- randomRIO (0,1) :: IO Int
+	if (isShipPlaceable h x y dir board) 
+	then placeShip h x y dir board 
+	else if (isShipPlaceable 0 x y dir board) then return board
+	else placeShips ships board where
+		ships = [h] ++ t
+		dir = False
 		
-getX = do
-       putStrLn "Enter a positive X value?"
-       line <- getLine
-       let parsed = reads line :: [(Integer, String)] in
-         if length parsed == 0
-         then getX'
-         else let (x, _) = head parsed in
-           if x > 0 
-           then return x
-           else getX'
-       where
-         getX' = do
-           putStrLn "Invalid input!"
-           getX
-		   
-getY = do
-       putStrLn "Enter a positive Y value?"
-       line <- getLine
-       let parsed = reads line :: [(Integer, String)] in
-         if length parsed == 0
-         then getY'
-         else let (y, _) = head parsed in
-           if y > 0 
-           then return y
-           else getY'
-       where
-         getY' = do
-           putStrLn "Invalid input!"
-           getY
+-- getX prompt = do
+       -- putStrLn ("Enter a positive" ++ prompt ++ "value?")   
+       -- line <- getLine
+       -- let parsed = reads line :: [(Integer, String)] in
+         -- if length parsed == 0
+         -- then getX'
+         -- else let (x, _) = head parsed in
+           -- if x > 0 
+           -- then return x
+           -- else getX'
+       -- where
+         -- getX' = do
+           -- putStrLn "Invalid input!"
+           -- getX
